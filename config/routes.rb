4cda350da737553e2e07c206cea48859dd6fb3ab
Richard Ortega <richardjortega@gmail.com>
2012-06-30 17:44:47 -0500

    stopping at listing 11.29

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ SampleApp2::Application.routes.draw do
       get :following, :followers
     end
   end
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   
   root to: 'static_pages#home'
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -110,6 +110,17 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the follower page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
       end
     end
 
